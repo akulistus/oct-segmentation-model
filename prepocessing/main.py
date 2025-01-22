5ab5aee9a0d14eb0f7f024f1cfe82eccb37bd4e3
Eugeny <akulistus555@gmail.com>
2025-01-23 01:43:36 +0300

    tried dynamic noise calculation

--- prepocessing/main.py
+++ prepocessing/main.py
@@ -1,24 +1,20 @@
 import cv2
+import numpy as np
+from skimage.util import img_as_float
+from skimage.restoration import estimate_sigma
 from bm3d import bm3d, BM3DStages
 
-# Load and preprocess the image
-test_image = "./data/test_image.jpeg"
+test_image = "./data/test_image2.jpeg"
 image = cv2.imread(test_image)
 
-# Convert to grayscale
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+normalized_image = img_as_float(gray_image)
 
-# Normalize pixel values to [0, 1]
-normalized_image = gray_image / 255.0
-
-# Apply BM3D denoising
-sigma_psd = 50 / 255.0  # Adjust sigma_psd for normalized scale
+sigma_psd = np.mean(estimate_sigma(normalized_image)) * 10
 denoised_image = bm3d(normalized_image, sigma_psd=sigma_psd, stage_arg=BM3DStages.ALL_STAGES)
 
-# Denormalize back to [0, 255] and convert to uint8
 denoised_image = (denoised_image * 255).astype('uint8')
 
-# Display the result
 cv2.imshow("Denoised Image", denoised_image)
 # cv2.imwrite("test.jpeg", denoised_image)
 cv2.waitKey(0)
