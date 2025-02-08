838f532496250ecd465c591cf5c908ddeb026c62
Eugeny <akulistus555@gmail.com>
2025-02-08 17:52:05 +0300

    complete preprocess

--- prepocessing/main.py
+++ prepocessing/main.py
@@ -11,30 +11,17 @@ files = os.listdir(dir)
 for file in files: 
     image = cv2.imread(f"{dir}/{file}")
     converted_image = prep.convert_to_gray(image)
-    # equalized_image = cv2.equalizeHist(converted_image)
-    # test1 = prep.fill_whites_with_noise(converted_image)
     filtered_image = prep.smooth_image(converted_image)
     sobel_image = prep.sobel(filtered_image)
     ret2, binary = prep.threshold(sobel_image)
     element = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
     binary = prep.morph_open(binary, element)
     binary = prep.morph_close(binary, element)
-    contours, hierarchy = cv2.findContours(binary, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    contours = prep.get_contours(binary)
+    resized = prep.resize_image(image, contours)
+    denoised_image = prep.remove_noise(resized)
 
-    approved_cnts = []
-    for cnt in contours:
-        contourArea = cv2.contourArea(cnt)
-        if (contourArea > 250):
-            x, y, w, h = cv2.boundingRect(cnt)
-            if (w < image.shape[1] / 4):
-                approved_cnts.append(cnt)
-            elif (not prep.is_line(cnt)) :
-                approved_cnts.append(cnt)
-    
-    x, y, w, h = cv2.boundingRect(np.vstack(approved_cnts))
-    cv2.rectangle(image, (x, y), (x + w, y + h + 20), (0, 255, 0), 2)
-
-    cv2.imwrite(f"result/{file}", image)
+    cv2.imwrite(f"result/{file}", denoised_image)
 # # cv2.imshow("Denoised Image", image)
 # # cv2.waitKey(0)
 # cv2.destroyAllWindows()
--- prepocessing/preprocessor.py
+++ prepocessing/preprocessor.py
@@ -16,22 +16,28 @@ class Preprocessor:
         return edges
 
     def remove_noise(self, image):
-        sigma = np.mean(estimate_sigma(image, average_sigmas=True)) * 10
+        sigma = np.mean(estimate_sigma(image, average_sigmas=True, channel_axis=-1)) * 10
         denoised_image = bm3d(image, sigma_psd=sigma, stage_arg=BM3DStages.ALL_STAGES)
         return denoised_image
     
-    def resize_image(self, image):
-        pass
+    def resize_image(self, image, contours):
+        x, y, w, h = cv2.boundingRect(np.vstack(contours))
+        cropped = image[y:y+h, x:x+w]
+        resized = cv2.resize(cropped, (200, 200))
+        return resized
 
-    def fill_whites_with_noise(self, image):
-        image_copy = image.copy()
-        _, mask = cv2.threshold(image, 200, 255, cv2.THRESH_BINARY)
-        mask_blurred = cv2.medianBlur(mask, 101)
-        mean_noise = estimate_sigma(image.astype(np.float32), average_sigmas=True) * 10
-        noise = np.random.normal(mean_noise, 50, image.shape).astype(np.uint8)
-        image_copy = (image + noise * (mask_blurred / 255.0)).astype(np.uint8) 
-        inpainted = cv2.inpaint(image_copy, mask_blurred, inpaintRadius=150, flags=cv2.INPAINT_TELEA)
-        return inpainted
+    def get_contours(self, image):
+        contours, hierarchy = cv2.findContours(image, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        approved_cnts = []
+        for cnt in contours:
+            contourArea = cv2.contourArea(cnt)
+            if (contourArea > 250):
+                x, y, w, h = cv2.boundingRect(cnt)
+                if (w < image.shape[1] / 4):
+                    approved_cnts.append(cnt)
+                elif (not self.is_line(cnt)) :
+                    approved_cnts.append(cnt)
+        return approved_cnts
     
     def is_line(self, contour):
         [vx, vy, x0, y0] = cv2.fitLine(contour, cv2.DIST_L2, 0, 0.01, 0.01)
@@ -45,7 +51,7 @@ class Preprocessor:
 
     def smooth_image(self, image):
         image_medianBlur = cv2.medianBlur(image, 21)
-        image_medianBlur = cv2.bilateralFilter(image_medianBlur, 11, 100, 100)
+        image_medianBlur = cv2.bilateralFilter(image_medianBlur, 11, 150, 150)
         return image_medianBlur
     
     def convert_to_gray(self, image):
@@ -56,9 +62,6 @@ class Preprocessor:
 
     def threshold(self, image):
         return cv2.threshold(image, 100, 255, cv2.THRESH_BINARY)
-    
-    def get_bounding_box(self, image):
-        pass
 
     def morph_open(self, image, element):
         binary = cv2.morphologyEx(image, cv2.MORPH_OPEN, element)
